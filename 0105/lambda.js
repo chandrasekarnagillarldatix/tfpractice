8679658b8a3b399f877260368541e5081d381a23
Chandrasekar Nagilla <chandrasekar.nagilla@rldatix.com>
2023-05-23 11:29:38 +0530

    Working Code

--- 0105/lambda.js
+++ 0105/lambda.js
@@ -1,10 +1,47 @@
-import {nanoid} from 'nanoid'
+import { MongoClient, ServerApiVersion } from 'mongodb';
+/*
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const AWS_SESSION_TOKEN = process.env.AWS_SESSION_TOKEN;
 
-export const handler = async (event) => {
-  console.log('Event: ', JSON.stringify(event));
+const uri = "mongodb+srv://<AWS access key>:<AWS secret key>@cluster0.jpelm7r.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority&authMechanismProperties=AWS_SESSION_TOKEN:<session token (for AWS IAM Roles)>"
+    .replace("<AWS access key>", AWS_ACCESS_KEY_ID)
+    .replace("<AWS secret key>", AWS_SECRET_ACCESS_KEY)
+    .replace("<session token (for AWS IAM Roles)>",AWS_SESSION_TOKEN);
+*/
 
-  return {
-    status: 200,
-    body: `Hello world! Your token is: ${nanoid()}`
+const uri = "mongodb+srv://cluster0.jpelm7r.mongodb.net/DBNAME?authSource=$external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export async function run() {
+  try {
+    console.log("Inside TRY");
+    // Connect the client to the server    (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("sample_analytics").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const db = client.db("sample_analytics");
+    let collection = db.collection('customers');
+
+    let query = { username: 'fmiller' }
+    let res = await collection.findOne(query);
+
+    console.log(res);
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
+run().catch(console.dir);
--- 0105/lambda.tf
+++ 0105/lambda.tf
@@ -1,12 +1,13 @@
 resource "aws_lambda_function" "simple_lambda" {
-    function_name = "Oregon_Lambda"  
+    function_name = "Mongo_Lambda"  
     role = aws_iam_role.iam_for_lambda.arn
     filename      = "lambda_function_payload.zip"
-    handler       = "index.test"
+    handler       = "lambda.run"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "nodejs16.x"
+  timeout = 15
 
   environment {
     variables = {
@@ -18,10 +19,48 @@ resource "aws_lambda_function" "simple_lambda" {
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "lambda.js"
   output_path = "lambda_function_payload.zip"
+  //source_file = "lambda.js"
+  //source_dir = "./node_modules/"
+
+  source {
+    content = data.template_file.mainjs.rendered
+    filename = "lambda.js"
+  }
+  source {
+    content = data.template_file.package.rendered
+    filename = "package.json"
+  }
+  #   source {
+  #   //content = data.template_file.nodemodules.rendered
+  #   content = "${file(base64encode("node_modules.zip"))}".filesha256
+  #   filename = "node_modules.zip"
+  # }
+  # depends_on = [ data.archive_file.nodemodules ]
 }
 
+data "template_file" "mainjs" {
+  template = "${file("lambda.js")}"
+}
+data "template_file" "package" {
+  template = "${file("package.json")}"
+}
+
+
+
+# data "template_file" "nodemodules" {
+#   template = "${filebase64("node_modules.zip")}"
+#   //template = data.archive_file.nodemodules.output_path
+#   depends_on = [ 
+#     data.archive_file.nodemodules
+#     ]
+# }
+
+# data "archive_file" "nodemodules" {
+#   type = "zip"
+#   source_dir = "./node_modules/"
+#   output_path = "node_modules.zip"
+# }
 
 data "aws_iam_policy_document" "assume_role" {
   statement {
@@ -35,7 +74,6 @@ data "aws_iam_policy_document" "assume_role" {
     actions = ["sts:AssumeRole"]
   }
 }
-
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
